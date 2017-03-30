5ea4b5da59643b57af2da2338c94bbf9ea34b88f
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-30 15:56:45 -0400

    Updates the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,4 @@ Rails.application.routes.draw do
   resources :listings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-
-  root "listings#index"
 end
