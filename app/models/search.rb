5196dfc7217a1e09b8f1ab6ef7750bc46a904630
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-31 08:36:37 -0400

    Adds more queries to the keywords search

--- app/models/search.rb
+++ app/models/search.rb
@@ -8,7 +8,7 @@ class Search < ApplicationRecord
 private
   def advanced_search
     listings = Listing.order(:color)
-    listings = listings.where(["color LIKE ?", "#{keyword}"]) if keyword.present?
+    listings = listings.where(["model ILIKE ? or make ILIKE ? or color ILIKE ?", "#{keyword}", "#{keyword}", "#{keyword}"]) if keyword.present?
     listings = listings.where(vehicle_type: vehicle_type) if vehicle_type.present?
     listings = listings.where(["price >= ?", min_price]) if min_price.present?
     listings = listings.where(["price <= ?", max_price]) if max_price.present?
