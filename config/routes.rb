2b33034c21b6ea0b7f0fa345beb21c393f51e618
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-31 09:39:50 -0400

    Adds new field params to the devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :listings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
+
+  root 'listings#index'
 end
