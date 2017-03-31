822c3dd2fe7b134f200dcde97d43fa603196d846
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-31 08:12:42 -0400

    Adds relation between listing and user and adds the before action to the listing controller

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,5 @@ class Listing < ApplicationRecord
   VEHICLES = [ "", "Motorcycle", "Truck", "Car", "RV" ]
 
   attachment :vehicle_image
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   SELLERS = [ "", "Dealer", "Broker", "Private Party" ]
+
+  has_many :listings
 end
