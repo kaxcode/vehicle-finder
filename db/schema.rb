98d878fd5383adddfeed21d949e7da92d233a901
Kenia Valladarez <keniavalladarez@gmail.com>
2017-03-31 08:08:52 -0400

    Adds user _id reference to the listing table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331003257) do
+ActiveRecord::Schema.define(version: 20170331120751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20170331003257) do
     t.datetime "updated_at",       null: false
     t.string   "vehicle_type"
     t.string   "vehicle_image_id"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_listings_on_user_id", using: :btree
   end
 
   create_table "refile_attachments", force: :cascade do |t|
@@ -69,4 +71,5 @@ ActiveRecord::Schema.define(version: 20170331003257) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "listings", "users"
 end
